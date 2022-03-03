6020c00e30f4d9aed54d044c1d8a4d2d5654170b
Atis Dubrovskis <atisdubrovskis@protonmail.com>
2022-03-03 19:32:34 +0200

    Kautko saku

--- Spēle/main.c
+++ Spēle/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    printf(" - - -- - -- -- -- - -- -- -- --- -- -- - -- - -- -- -- - -- - --- --- -- -- ---");
-    printf("OXXXXXO");
+void main() {
+    printf("Izvēlne");
+    
+   int a,b,c;
+   printf( "\nIevadi skaitli 1 : ");
+
+    a = scanf("%d", &a);
+   
+   if (a == 1) {
+
+       int upe;
+       printf("\nAtbilde ir 1.");
+       system("clear"); //*nix
+       
+       printf("Kāda ir garākā upe Latvijā?");
+        upe = scanf("%d", &upe);
+            if (upe == "Gauja") {
+                printf("Pareizi.");
+                return 0;
+
+            } else {
+                printf("Nepareizi!");
+            }
+
+   }
+
+    if (a > 1) {
+
+        printf("\nAtbilde ir lielāka par 1. %d", a);
+
+        return 0;
+    }
 }
