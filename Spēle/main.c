2d132901cc0d4f995fe2be50e0ebe99ce7f40506
Atis Dubrovskis <atisdubrovskis@protonmail.com>
2022-03-08 18:36:17 +0200

    pieviono

--- Spēle/main.c
+++ Spēle/main.c
@@ -1,37 +1,91 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
-    printf("Izvēlne");
-    
-   int a;
-   printf( "\nIevadi skaitli 1 : ");
+        
+        //
+        // funkcijas
+        //
+        
+        //Lai dzēstu ekrānu pēc izvades
+        void clearscr(void) {
+// ja windows
+#ifdef _WIN32
+    system("cls");
+//ja unix, linux vai mac os
+#elif defined(unix) || defined(__unix__) || defined(__unix) || (defined(__APPLE__) && defined(__MACH__))
+    system("clear");
+// ja nav atbalstīts - vai nu nav definēta vai nav atbalstīta
+#else
+    #error "OS nav atbalstīta"
+#endif
+}
 
-    a = scanf("%d", &a);
-   
-   if (a == 1) {
 
-       char gauja =  (int) "gauja";
-       char upe[50];
-       int result;
-       printf("\nAtbilde ir 1.");
-       system("clear"); //*nix
+
+
+
+
+        void sakt();   
+
+       void palidziba() {  
+     
+     int izvele;  
+  
+     printf("\n\n ************************* Spēles pamācība *************************");  
+     printf("\n -------------------------------------------------------------------------");  
+     printf("\n .................... Spēle (aizvietot)...........\n");  
+     printf("\n >> Spēlē ir tūrisma apskates punkti un uzlādes stacijas, kurās ir jātbild uz jautājumu, lai uzlādētu elektroauto un tiktu tālāk. \n");
+     printf("\n >> Uz jautājumiem ir jāatbild vai nu apskates objektos vai uzlādes stacijās. \n");
+     printf("\n >> Uz jautājumiem jāatbild ievadot atbildi vai atbildes ciparu, ja dotas. \n"); 
+     printf("\n >> Pēc katras otrās uzlādes stacijas ir iespējama laikapstākļu maiņa.  ");  
+     printf("\n >> Elektroauto bez uzlādes saulainos vai mākoņainos laikapstākļos spēj nobraukt 140 km un lietainā vai vētrā spēj nobraukt 80 km");  
+  
+    printf("\n\n ************************* Veiksmi! *************************\n\n");  
+  
+  
+    printf("\n Turpināt vai iziet? 1, lai turpinātu, 0, lai izietu \n: ");  
+    scanf("%d",&izvele);  
+  
+    if(izvele==1)  
+    {  
+        start();  
+    }  
+    else  
+    {  
+        exit(1);  
+    }  
+  
+}  
        
-       printf("Kāda ir garākā upe Latvijā?");
-       result = scanf("%s",upe);
-            if (result == gauja) {
+       
+    palidziba();   
+       
+       
+       
+       
+       
+       
+       
+       char upe[10];
+       
+       printf("Kāda ir garākā upe Latvijā?  \n:");
+       scanf("%s",upe);
+            if ( strcasecmp( upe, "gauja" ) == 0) {
                 printf("Pareizi.");
-                return 0;
+                    
+         //char upe[10];
+       
+       printf("Kāda ir garākā upe Latvijā?  \n:");
+       scanf("%s",upe);
+            if ( strcasecmp( upe, "gauja" ) == 0) {
+                printf("Pareizi.");
+                clearscr();
 
             } else {
-                printf("Nepareizi! Atbilde - %s, Upe - %s", gauja, upe);
-            }
+                printf("Nepareizi! Atbilde - gauja, Upe - %s", upe);
+                return 0;
+            };
 
-   }
 
-    if (a > 1) {
-
-        printf("\nAtbilde ir lielāka par 1. %d", a);
-
-        return 0;
-    }
-}
